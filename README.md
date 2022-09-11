3c594e287a8b19c443e2d7f25f41032fb7166f5a
Dan Joar Heimvik <danjoar@gmail.com>
2022-09-11 21:04:43 +0200

    working prototype + readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Arduino Face Tracking
+## Introduction:
+This is a simple project using facial recognition, an arduino and a web camera to track and follow a face. I'm using the
+[CascadeClassifier](https://docs.opencv.org/4.x/d1/de5/classcv_1_1CascadeClassifier.html) from the open-source library 
+[OpenCV](https://github.com/opencv/opencv) for Python. The coordinates from the detection gets fed into an arduino where
+it gets translated to motor-controls, pointing the camera at the face.
+
+[pic]()
+
+## Dependencies:
+* [Pyserial](https://pypi.org/project/pyserial/)
+* [OpenCV](https://pypi.org/project/opencv-python/)
+
+## TODO:
+- [ ] Integration with OV7670 camera module
+- [ ] Model and print custom parts for servos and camera
+- [ ] Integrate laser module to blind guests (???)
+
+## Arduino schematics:
+[schem_img]()
---
+++ python/faceDetection.py
@@ -0,0 +1,34 @@
+import cv2
+import serial
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+capture = cv2.VideoCapture(0)
+arduino_serial = serial.Serial('COM3', 9600, timeout=0.1)
+
+while capture.isOpened():
+    _, frame = capture.read()
+    # Mirror the frame from camera
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Detect face
+    face = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for x, y, w, h in face:
+        coords = 'X{0:d}Y{1:d}'.format((x+w//2), (y+h//2))
+        print(coords)
+        # Write to arduino
+        arduino_serial.write(coords.encode('utf-8'))
+
+        # Plot face
+        cv2.circle(frame, (x+w//2, y+h//2), 2, (0,255,0), 2)
+        # Plot region
+        cv2.rectangle(frame, (x, y), (x+w,y+h), (0,0,255), 3)
+    # Plot center region
+    cv2.rectangle(frame,(640//2-30,480//2-30),
+                  (640//2+30,480//2+30),
+                  (255,255,255),3)
+    # Display image
+    cv2.imshow('img', frame)
+    if cv2.waitKey(10)&0xFF == ord('q'):
+        break
+capture.realease()
+cv2.destroyAllWindows()
